c8059522860fc7e509a54b9fccbc9dbf90681bb4
Michael Pingel <mdpingel@gmail.com>
2021-01-24 22:51:47 -0800

    add functionally to add and remove contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignID();
+  this.contacts[contact.id] = contact;
+}
+
+AddressBook.prototype.assignID = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+}
+
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
